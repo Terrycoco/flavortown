143f50f4f62e4b57448af4ad1bb2dd10efa252a2
Terry <terrymarr280@gmail.com>
2021-04-04 16:19:43 -0700

    force port

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const db = require("./db.js");
 app.use(express.json());
 app.use(cors());
 
+let PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV == 'development') {
+  PORT = 5000; //force
+}
+ 
+
+
 
 // tests connection and returns Postgres server version,
 // if successful; or else rejects with connection error:
@@ -38,5 +45,5 @@ app.get('/items', (req, res, next) => {
     })
 });
 
-const PORT = process.env.EXPRESS_PORT || process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
